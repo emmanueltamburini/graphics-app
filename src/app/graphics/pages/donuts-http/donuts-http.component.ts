3f7219690710b79374a1c12924729076e887a03c
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-10-18 23:45:12 -0400

    feature/sp22-51775: donuts http has been added

--- src/app/graphics/pages/donuts-http/donuts-http.component.ts
+++ src/app/graphics/pages/donuts-http/donuts-http.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { GraphicsService } from '../../services/graphics.service';
 
 @Component({
   selector: 'app-donuts-http',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonutsHttpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  graphicsService: GraphicsService) { }
 
   ngOnInit(): void {
+    this.graphicsService.getDataDonutsData()
+      .subscribe(({labels, values}) => {
+
+        this.doughnutChartData.labels = labels;
+        this.doughnutChartData.datasets.push(
+          {
+            data: values,
+            backgroundColor: ['#9673FF', '#686BE8', '#7FA3FF', '#68AEE8', '#73E4FF']
+          }
+        );
+      });
+  }
+
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: [],
+    datasets: [],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public doughnutChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    aspectRatio: 2
+  };
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
 }
--- src/app/graphics/services/graphics.service.ts
+++ src/app/graphics/services/graphics.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraphicsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getData(): Observable<Object> {
+    return this.http.get('http://localhost:3000/graph');
+  }
+
+  public getDataDonutsData():
+    Observable<{
+      labels: string[];
+      values: any[];}
+    > {
+    return this.http.get('http://localhost:3000/graph')
+      .pipe(
+        delay(1500),
+        map(data => {
+          const labels = Object.keys(data);
+          const values = Object.values(data);
+          return { labels, values };
+        })
+      );
+  }
 }
