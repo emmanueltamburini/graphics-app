52949528f6dabc531b9b08d5ed2761dcbeac9115
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-10-18 23:09:20 -0400

    donuts graph has been added

--- src/app/graphics/pages/donuts/donuts.component.ts
+++ src/app/graphics/pages/donuts/donuts.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-donuts',
   templateUrl: './donuts.component.html',
   styleUrls: ['./donuts.component.css']
 })
-export class DonutsComponent implements OnInit {
+export class DonutsComponent {
 
-  constructor() { }
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [ 350, 450, 100 ],
+        backgroundColor: ['#7245ED', '#4848F7', '#4C73E0']
+      }
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
-  ngOnInit(): void {
+  public doughnutChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    aspectRatio: 2,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  };
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
 }
