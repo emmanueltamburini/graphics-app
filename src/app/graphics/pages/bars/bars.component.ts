863fac4846dc30152ee3ab1abf4c6e7f369e8f2d
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-10-18 22:11:44 -0400

    bars has been added

--- src/app/graphics/pages/bars/bars.component.ts
+++ src/app/graphics/pages/bars/bars.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-bars',
   templateUrl: './bars.component.html',
   styleUrls: ['./bars.component.css']
 })
-export class BarsComponent implements OnInit {
+export class BarsComponent {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  constructor() { }
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public barChartType: ChartType = 'bar';
 
-  ngOnInit(): void {
+  public barChartData: ChartData<'bar'> = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#7245ED', hoverBackgroundColor: '#45CAED' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#4848F7', hoverBackgroundColor: '#45CAED' },
+      { data: [ 21, 12, 32, 12, 43, 23, 0 ], label: 'Series C', backgroundColor: '#4C73E0', hoverBackgroundColor: '#45CAED' }
+    ]
+  };
+
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+    ];
+
+    this.chart?.update();
   }
 
 }
